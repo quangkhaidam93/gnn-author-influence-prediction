666d20abc2786e51e969b9eec60a46722cf6931f
Khai Dam <quangkhaidam93@gmail.com>
2025-04-13 22:00:26 +0700

    feat: add average_precision_score

--- services/evaluate.py
+++ services/evaluate.py
@@ -5,6 +5,7 @@ from sklearn.metrics import (
     recall_score,
     f1_score,
     roc_auc_score,
+    average_precision_score,
 )
 import torch.nn.functional as F
 
@@ -21,9 +22,10 @@ def evaluate(model, data, mask):
     prec = precision_score(label.cpu(), pred.cpu(), zero_division=0)
     rec = recall_score(label.cpu(), pred.cpu(), zero_division=0)
     f1 = f1_score(label.cpu(), pred.cpu(), zero_division=0)
+    avg_prec = average_precision_score(label.cpu(), prob.cpu())
     try:
         auc = roc_auc_score(label.cpu(), prob.cpu())
     except ValueError:  # Handle cases where only one class is present in the mask
         auc = 0.5  # Or another appropriate default
 
-    return acc, prec, rec, f1, auc
+    return acc, prec, rec, f1, auc, avg_prec
